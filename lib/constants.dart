5c99a1d7b0e9987db04445e68f6eeb98c344855d
Brian Vasquez <Brian_07@live.com>
2022-10-21 15:48:46 -0600

    Welcome page and firebase setup

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = const Color.fromARGB(255, 31, 32, 39);
+Color textColor = const Color.fromARGB(255, 248, 136, 44);
---
+++ lib/views/welcome_page.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:foodu/constants.dart';
+
+class Welcome_Page extends StatelessWidget {
+  const Welcome_Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context)
+        .size; // This is provides the total width and height of our screen
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 300), // margin from the top of the screen
+        child: Column(
+          children: <Widget>[
+            Text(
+              "Welcome to Foodu",
+              style: TextStyle(color: textColor, fontSize: 35),
+            ),
+            Container(
+              width: size.width * 0.7,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(29),
+                child: FlatButton(
+                    // ignore: prefer_const_constructors
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                    color: textColor,
+                    onPressed: () {},
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: backgroundColor),
+                    )),
+              ),
+            ),
+            Container(
+              width: size.width * 0.7,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(29),
+                child: FlatButton(
+                    // ignore: prefer_const_constructors
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                    color: textColor,
+                    onPressed: () {},
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: backgroundColor),
+                    )),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
